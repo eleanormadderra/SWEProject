fc188c67382e9ff3292587d3a5a7d2586014771f
TheRyanMajd <rm97798@uga.edu>
2024-11-18 15:07:18 -0500

    Reenabled auth return to main page Bug with Leaflet reinitalization, will fix soon trust

--- frontend/bus-app/app/page.jsx
+++ frontend/bus-app/app/page.jsx
@@ -1,40 +1,42 @@
 'use client'
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from "../app/firebase/config";
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "../app/firebase/config";
 import MainPage from './components/ugathens-bus';
 
 export default function Home() {
   const [user] = useAuthState(auth);
   const router = useRouter()
   const userSession = sessionStorage.getItem('user');
-
-  console.log({user})
+  // console.log({user})
  
-  // if (!user && !userSession){
-  //   router.push('/sign-up')
-  // }
+  if (!user && !userSession){
+    router.push('/sign-up')
+  }
 
   useEffect(() => {
-    // if (!user) {
-    //   router.push('/sign-up');  // Redirect if no user is authenticated
-    // }
+    if (!user && !userSession) {
+      router.push('/sign-up');  // Redirect if no user is authenticated
+    }
   }, [user, router]);  // Only run the effect when the user state changes
   
+//TOOD: update and put as component
 
   return (
+    // Will need to update this cause this is sloppy 
     <>
       <button 
         onClick={() => {
-          signOut(auth);
+        //  signOut(auth); // ????
           sessionStorage.removeItem('user');
+          router.push('/sign-up');
         }}
       >
         Log out
       </button>
       <div>
-        <MainPage />
+        <MainPage /> 
       </div>
     </>
   );
